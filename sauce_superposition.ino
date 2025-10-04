30fde748fdecb10aee565d5d2ee7e90e8fecb117
xcodian <44289933+xcodian@users.noreply.github.com>
2025-10-04 16:41:44 +1000

    remove comments

--- sauce_superposition.ino
+++ sauce_superposition.ino
@@ -1,25 +1,3 @@
-// #include <Servo.h>
-
-// Servo servo;  // create servo object to control a servo
-
-// void setup() {
-//   servo.attach(9);  // attaches the servo on pin 9 to the servo objectư
-//   servo.write(0);   // rotate slowly servo to 0 degrees immediately
-// }
-
-// void loop() {
-  // for (int angle = 0; angle <= 180; angle += 1) {  // rotate slowly from 0 degrees to 180 degrees, one by one degree
-  //   // in steps of 1 degree
-  //   servo.write(angle);  // control servo to go to position in variable 'angle'
-  //   delay(10);         // waits 10ms for the servo to reach the position
-  // }
-
-  // for (int angle = 180; angle >= 0; angle -= 1) {  // rotate from 180 degrees to 0 degrees, one by one degree
-  //   servo.write(angle);                        // control servo to go to position in variable 'angle'
-  //   delay(10);                               // waits 10ms for the servo to reach the position
-  // }
-// }
-
 #include <WiFi.h>
 #include <ArduinoOTA.h>
 #include <WebServer.h>
